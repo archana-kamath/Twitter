6c762a81730c2afe3ee47c94ae0bb7fefe7486bc
Archana Kamath <68199391+archana-kamath@users.noreply.github.com>
2021-09-27 05:08:25 +0530

    Updating name

--- twitter-demonstration/src/main/java/com/sjsu/twitterdemonstration/TwitterDemonstrationApplication.java
+++ twitter-demonstration/src/main/java/com/sjsu/twitterdemonstration/TwitterDemonstrationApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
 /**
- * @author archa
+ * @author archa - Archana
  *
  */
 @SpringBootApplication
